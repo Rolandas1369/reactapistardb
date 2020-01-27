28113f332743c7ee165be65337c2e551cac6b00a
Rolandas <rolandaswbu@gmail.com>
2020-01-27 15:03:43 +0200

    api class client

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -7,18 +7,59 @@
 
 // 3. moving all code to class
 class SwapiService {
-   async getResource () {
-        const res = await fetch(url);
+
+    _apiBase = 'https://swapi.co/api';
+
+   async getResource (url) {
+        const res = await fetch(`${this._apiBase}${url}`);
     
         // checking if return status is okay
         if (!res.ok) {
-            throw new Error(`Could not fetch ${url}` + `, received ${res.status}`)
+            throw new Error(`Could not fetch ${url}, received ${res.status}`)
         }
         
         return await res.json();
     };
+
+    async getAllPeople(){
+        // wait for result
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id){
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getAllPlanets(){
+        // wait for result
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id){
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarships(){
+        // wait for result
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarship(id){ 
+        return this.getResource(`/starships/${id}`);
+    }
+
 }
 
+
+//const swapi = new SwapiService();
+// swapi.getAllPeople().then((people) => {
+//     people.forEach(element => {
+//         console.log(element.name)
+//     });
+// });
 
 // more nicer way
 // const getResource = async (url) => {
